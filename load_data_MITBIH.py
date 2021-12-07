740054d8de55a70a66417b0e4b04bd64d466c8b3
carolinefreyer <carolinefreyer@gmail.com>
2021-12-07 16:22:42 +0100

    start of evaluation.

--- load_data_MITBIH.py
+++ load_data_MITBIH.py
@@ -3,32 +3,36 @@ import wfdb
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
-#we only consider two classes: normal and abnormal beats.
+
+# we only consider two classes: normal and abnormal beats.
 def classify_beat(symbol):
     if symbol == "N" or symbol == ".":
         return 0
     else:
         return 1
 
-def get_beat(signal, peak, next_peak, freq):
+
+def get_beat_old(signal, peak, freq):
     # length of heart beat is now randomly set as 3 seconds per side.
     window_size = 3
     window_one_side = window_size * freq
     beat_start = peak - window_one_side
     beat_end = peak + window_one_side + 1
-    #this cuts off the last beat incase its not whole
+    # this cuts off the last beat incase its not whole
     if beat_end < signal.shape[0]:
         return signal[beat_start:beat_end]
     else:
         return np.array([])
+
 
 def normalise(record):
     # centering and scaling happens independently on each signal
     scaler = StandardScaler()
     return scaler.fit_transform(record.p_signal)
 
-#Find beat that is labelled.
-def label_segment(record, annotation):
+
+# Find beat that is labelled.
+def label_clean_segments(record, annotation):
     heart_beats = []
     heart_beats_x = []
     labels = []
@@ -38,13 +42,15 @@ def label_segment(record, annotation):
 
     start = 0
     for i, i_sample in enumerate(ann_sample):
+        if ann_symbol[i] == "+":
+            continue
         label = classify_beat(ann_symbol[i])
-        if i != len(ann_sample)-1:
-            end = ann_sample[i+1]-10
+        if i != len(ann_sample) - 1:
+            end = ann_sample[i + 1] - 10
         else:
-             end = len(record.p_signal)
+            end = len(record.p_signal)
         beat = record.p_signal[start:end]
-        beat_x = range(start,end)
+        beat_x = range(start, end)
         start = end + 1
         if label is not None and beat.size > 0:
             heart_beats.append(beat)
@@ -56,21 +62,21 @@ def label_segment(record, annotation):
 
 def plot_data_basic(record, annotation):
     wfdb.plot_wfdb(record=record, annotation=annotation, plot_sym=True,
-                   time_units = 'seconds', title = 'MIT-BIH Record 100',
-                    figsize = (10, 4), ecg_grids = 'all')
+                   time_units='seconds', title='MIT-BIH Record 100',
+                   figsize=(10, 4), ecg_grids='all')
 
 
-def plot_data(record,annotation):
-    heart_beats, heart_beats_x, labels = label_segment(record, annotation)
-    colours =[]
+def plot_data(record, annotation):
+    heart_beats, heart_beats_x, labels = label_clean_segments(record, annotation)
+    colours = []
     for i in labels:
         if i == 0:
             colours.append("k")
         else:
             colours.append("r")
     fig, axs = plt.subplots(2)
-    x_labels = [i for i in range(0,record.p_signal.shape[0], 10*record.fs)]
-    x_labels_values = [10*i for i in range(len(x_labels))]
+    x_labels = [i for i in range(0, record.p_signal.shape[0], 10 * record.fs)]
+    x_labels_values = [10 * i for i in range(len(x_labels))]
     fig.suptitle("MIT-BIH Arrhythmia Database: Sample 100")
     signal1 = [[item[0] for item in heart_beat] for heart_beat in heart_beats]
     signal2 = [[item[1] for item in heart_beat] for heart_beat in heart_beats]
@@ -79,9 +85,10 @@ def plot_data(record,annotation):
         axs[1].plot(heart_beats_x[i], signal2[i], color=c)
     axs[0].set(ylabel=record.sig_name[0])
     axs[0].set_xticks(x_labels, x_labels_values)
-    axs[1].set(xlabel= "seconds",ylabel=record.sig_name[1])
+    axs[1].set(xlabel="seconds", ylabel=record.sig_name[1])
     axs[1].set_xticks(x_labels, x_labels_values)
     plt.show()
+
 
 def load_mit_bih_data(plot, sampto):
     record = wfdb.rdrecord('./data/sample_100/100', sampto=sampto)
--- random_projection_MITBIH.py
+++ random_projection_MITBIH.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn import random_projection
 
 
-def random_projection(data_points, k, norm_persevation):
+def random_projection_single(data_points, k, norm_perservation):
     d = data_points.shape[1]
 
     outlier_scores = []
@@ -12,35 +12,60 @@ def random_projection(data_points, k, norm_persevation):
     for point in data_points:
         point_proj = (1 / np.sqrt(d) * R) @ point
         point_reconstruct = (1 / np.sqrt(d) * R.T) @ point_proj
-        if norm_persevation:
+        if norm_perservation:
             point_reconstruct = np.sqrt(d / k) * point_reconstruct
         outlier_score = np.linalg.norm(point - point_reconstruct)
         outlier_scores.append(outlier_score)
     return outlier_scores
 
 
-def main():
-    record, annotation = load_data_MITBIH.load_mit_bih_data(True, 20000)
-    # Determine dimension k
-    outlier_scores = random_projection(record.p_signal, 1, False)
-    heart_beats, heart_beats_x, labels = load_data_MITBIH.label_segment(record, annotation)
+def average_beat_score(beat_outlier_scores, labels):
+    beat_scores_avg = [sum(score)/len(score) for score in beat_outlier_scores]
 
-    beat_outlier_scores = []
-    for x in heart_beats_x:
-        beat_scores = [outlier_scores[i] for i in x]
-        # beat_scores_avg = sum(beat_scores)/len(beat_scores)
-        beat_outlier_scores.append(max(beat_scores))
+    #Basic Evaluation
+    normal_avg = []
+    outlier_avg = []
+    for i in range(len(labels)):
+        if labels[i] == 0:  # normal
+            normal_avg.append(beat_scores_avg[i])
+        else:
+            outlier_avg.append(beat_scores_avg[i])
+
+    print("Normal average:", sum(normal_avg) / len(normal_avg))
+    print("Outlier average:", sum(outlier_avg) / len(outlier_avg))
+
+
+def max_beat_score(beat_outlier_scores, labels):
+
+    beat_scores_max = [max(score) for score in beat_outlier_scores]
 
     normal_avg = []
     outlier_avg = []
     for i in range(len(labels)):
         if labels[i] == 0:  # normal
-            normal_avg.append(beat_outlier_scores[i])
+            normal_avg.append(beat_scores_max[i])
         else:
-            outlier_avg.append(beat_outlier_scores[i])
+            outlier_avg.append(beat_scores_max[i])
 
     print("Normal average:", sum(normal_avg) / len(normal_avg))
     print("Outlier average:", sum(outlier_avg) / len(outlier_avg))
+
+
+def get_beat_scores(record, annotation):
+    outlier_scores = random_projection_single(record.p_signal, 1, False)
+    heart_beats, heart_beats_x, labels = load_data_MITBIH.label_clean_segments(record, annotation)
+    beat_outlier_scores = []
+    for x in heart_beats_x:
+        beat_scores = [outlier_scores[i] for i in x]
+        beat_outlier_scores.append(beat_scores)
+    return beat_outlier_scores, labels
+
+def main():
+    record, annotation = load_data_MITBIH.load_mit_bih_data(False, None)
+    beat_outlier_scores, labels = get_beat_scores(record,annotation)
+    average_beat_score(beat_outlier_scores, labels)
+    max_beat_score(beat_outlier_scores, labels)
+
 
     normal_percentage = 1 - (sum(labels) / len(labels))
     # print('normal percentage:', normal_percentage)
